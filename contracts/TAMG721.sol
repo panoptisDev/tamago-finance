5cfbfcc73b50f30d8be9a53a61be8c213587ae4e
tesol2y090 <gliese090@gmail.com>
2022-07-21 17:31:38 +0700

    add: tamg721 launchpad

--- contracts/TAMG721.sol
+++ contracts/TAMG721.sol
@@ -1,106 +1,352 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "erc721a/contracts/ERC721A.sol";
-import "erc721a/contracts/extensions/ERC721ABurnable.sol";
-import "erc721a/contracts/extensions/ERC721AQueryable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./lib/EIP712Whitelisting.sol";
+import "./lib/BlockBasedSale.sol";
 
-contract TAMG721 is ERC721A, ERC721AQueryable, ERC721ABurnable, Ownable {
-  using Strings for uint256;
+contract TAMG721 is
+  Ownable,
+  ERC721,
+  ERC721Enumerable,
+  EIP712Whitelisting,
+  BlockBasedSale,
+  ReentrancyGuard
+{
+  using Address for address;
+  using SafeMath for uint256;
 
-  address public minter;
-
-  string private baseURI;
-  string public baseExtension = ".json";
-  uint256 public maxSupply;
-  uint256 public randomHash;
-
-  event SetMinter(address indexed caller, address indexed minter);
-  event SetBaseURI(string OldBaseURI, string NewBaseURI);
-
-  modifier onlyMinter() {
-    require(msg.sender == minter, "You are not a minter");
-    _;
+  enum SaleState {
+    NotStarted,
+    PrivateSaleBeforeWithoutBlock,
+    PrivateSaleBeforeWithBlock,
+    PrivateSaleDuring,
+    PrivateSaleEnd,
+    PrivateSaleEndSoldOut,
+    PublicSaleBeforeWithoutBlock,
+    PublicSaleBeforeWithBlock,
+    PublicSaleDuring,
+    PublicSaleEnd,
+    PublicSaleEndSoldOut,
+    PauseSale,
+    AllSalesEnd
   }
+
+  bool public beneficiaryAssigned = false;
+
+  uint256 public seed = 0;
+
+  mapping(address => uint256) private _privateSaleClaimed;
+
+  string public _tokenBaseURI;
 
   constructor(
-    string memory _initBaseURI,
-    string memory _name,
-    string memory _symbol,
+    uint256 _privateSalePrice,
+    uint256 _publicSalePrice,
+    string memory name,
+    string memory symbol,
     uint256 _maxSupply,
-    uint256 _randomHash
-  ) ERC721A(_name, _symbol) {
+    SaleConfig memory privateSale,
+    SaleConfig memory publicSale
+  ) ERC721(name, symbol) BlockBasedSale(privateSale, publicSale) {
     maxSupply = _maxSupply;
-    randomHash = _randomHash;
-
-    setBaseURI(_initBaseURI);
-    setMinter(msg.sender);
+    publicSalePrice = _publicSalePrice;
+    privateSalePrice = _privateSalePrice;
   }
 
-  function _baseURI() internal view override returns (string memory) {
-    return baseURI;
+  function mintToken(uint256 amount, bytes calldata signature)
+    external
+    payable
+    nonReentrant
+    returns (bool)
+  {
+    require(!msg.sender.isContract(), "Contract is not allowed.");
+    require(
+      getState() == SaleState.PrivateSaleDuring ||
+        getState() == SaleState.PublicSaleDuring,
+      "Sale not available."
+    );
+
+    if (getState() == SaleState.PublicSaleDuring) {
+      require(amount <= maxPublicSalePerTx, "Mint exceed transaction limits.");
+      require(msg.value >= amount.mul(getPriceByMode()), "Insufficient funds.");
+      require(
+        totalSupply().add(amount).add(availableReserve()) <= maxSupply,
+        "Purchase exceed max supply."
+      );
+    }
+
+    if (getState() == SaleState.PrivateSaleDuring) {
+      require(isEIP712WhiteListed(signature), "Not whitelisted.");
+      require(amount <= maxPrivateSalePerTx, "Mint exceed transaction limits");
+      require(
+        totalPrivateSaleMinted.add(amount) <= privateSaleCapped,
+        "Purchase exceed private sale capped."
+      );
+
+      require(msg.value >= amount.mul(getPriceByMode()), "Insufficient funds.");
+    }
+
+    if (
+      getState() == SaleState.PrivateSaleDuring ||
+      getState() == SaleState.PublicSaleDuring
+    ) {
+      _mintToken(msg.sender, amount);
+      if (getState() == SaleState.PublicSaleDuring) {
+        totalPublicMinted = totalPublicMinted + amount;
+      }
+      if (getState() == SaleState.PrivateSaleDuring) {
+        _privateSaleClaimed[msg.sender] =
+          _privateSaleClaimed[msg.sender] +
+          amount;
+        totalPrivateSaleMinted = totalPrivateSaleMinted + amount;
+      }
+    }
+
+    return true;
   }
 
-  function setMinter(address _minter) public onlyOwner {
-    minter = _minter;
-    emit SetMinter(msg.sender, minter);
+  function setBaseURI(string memory baseURI) external onlyOwner {
+    _tokenBaseURI = baseURI;
   }
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    string memory _oldBaseURI = baseURI;
-    baseURI = _newBaseURI;
+  function getState() public view returns (SaleState) {
+    uint256 supplyWithoutReserve = maxSupply - maxReserve;
+    uint256 mintedWithoutReserve = totalPublicMinted + totalPrivateSaleMinted;
 
-    emit SetBaseURI(_oldBaseURI, baseURI);
+    if (
+      salePhase != SalePhase.None &&
+      overridedSaleState == OverrideSaleState.Close
+    ) {
+      return SaleState.AllSalesEnd;
+    }
+
+    if (
+      salePhase != SalePhase.None &&
+      overridedSaleState == OverrideSaleState.Pause
+    ) {
+      return SaleState.PauseSale;
+    }
+
+    if (
+      salePhase == SalePhase.Public &&
+      mintedWithoutReserve == supplyWithoutReserve
+    ) {
+      return SaleState.PublicSaleEndSoldOut;
+    }
+
+    if (salePhase == SalePhase.None) {
+      return SaleState.NotStarted;
+    }
+
+    if (
+      salePhase == SalePhase.Public &&
+      publicSale.endBlock > 0 &&
+      block.number > publicSale.endBlock
+    ) {
+      return SaleState.PublicSaleEnd;
+    }
+
+    if (
+      salePhase == SalePhase.Public &&
+      publicSale.beginBlock > 0 &&
+      block.number >= publicSale.beginBlock
+    ) {
+      return SaleState.PublicSaleDuring;
+    }
+
+    if (
+      salePhase == SalePhase.Public &&
+      publicSale.beginBlock > 0 &&
+      block.number < publicSale.beginBlock &&
+      block.number > privateSale.endBlock
+    ) {
+      return SaleState.PublicSaleBeforeWithBlock;
+    }
+
+    if (
+      salePhase == SalePhase.Public &&
+      publicSale.beginBlock == 0 &&
+      block.number > privateSale.endBlock
+    ) {
+      return SaleState.PublicSaleBeforeWithoutBlock;
+    }
+
+    if (
+      salePhase == SalePhase.Private &&
+      totalPrivateSaleMinted == privateSaleCapped
+    ) {
+      return SaleState.PrivateSaleEndSoldOut;
+    }
+
+    if (
+      salePhase == SalePhase.Private &&
+      privateSale.endBlock > 0 &&
+      block.number > privateSale.endBlock
+    ) {
+      return SaleState.PrivateSaleEnd;
+    }
+
+    if (
+      salePhase == SalePhase.Private &&
+      privateSale.beginBlock > 0 &&
+      block.number >= privateSale.beginBlock
+    ) {
+      return SaleState.PrivateSaleDuring;
+    }
+
+    if (
+      salePhase == SalePhase.Private &&
+      privateSale.beginBlock > 0 &&
+      block.number < privateSale.beginBlock
+    ) {
+      return SaleState.PrivateSaleBeforeWithBlock;
+    }
+
+    if (salePhase == SalePhase.Private && privateSale.beginBlock == 0) {
+      return SaleState.PrivateSaleBeforeWithoutBlock;
+    }
+
+    return SaleState.NotStarted;
+  }
+
+  function getStartSaleBlock() external view returns (uint256) {
+    if (
+      SaleState.PrivateSaleBeforeWithBlock == getState() ||
+      SaleState.PrivateSaleDuring == getState()
+    ) {
+      return privateSale.beginBlock;
+    }
+
+    if (
+      SaleState.PublicSaleBeforeWithBlock == getState() ||
+      SaleState.PublicSaleDuring == getState()
+    ) {
+      return publicSale.beginBlock;
+    }
+
+    return 0;
+  }
+
+  function getEndSaleBlock() external view returns (uint256) {
+    if (
+      SaleState.PrivateSaleBeforeWithBlock == getState() ||
+      SaleState.PrivateSaleDuring == getState()
+    ) {
+      return privateSale.endBlock;
+    }
+
+    if (
+      SaleState.PublicSaleBeforeWithBlock == getState() ||
+      SaleState.PublicSaleDuring == getState()
+    ) {
+      return publicSale.endBlock;
+    }
+
+    return 0;
+  }
+
+  function tokenBaseURI() external view returns (string memory) {
+    return _tokenBaseURI;
   }
 
   function tokenURI(uint256 tokenId)
     public
     view
-    virtual
-    override(ERC721A)
+    override(ERC721)
     returns (string memory)
   {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
+    require(tokenId < totalSupply() + 1, "Token not exist.");
 
-    string memory currentBaseURI = _baseURI();
+    return string(abi.encodePacked(_tokenBaseURI, tokenId, ".json"));
+  }
+
+  function availableReserve() public view returns (uint256) {
+    return maxReserve - totalReserveMinted;
+  }
+
+  function getMaxSupplyByMode() public view returns (uint256) {
+    if (getState() == SaleState.PrivateSaleDuring) return privateSaleCapped;
+    if (getState() == SaleState.PublicSaleDuring)
+      return maxSupply - totalPrivateSaleMinted - maxReserve;
+    return 0;
+  }
+
+  function getMintedByMode() external view returns (uint256) {
+    if (getState() == SaleState.PrivateSaleDuring)
+      return totalPrivateSaleMinted;
+    if (getState() == SaleState.PublicSaleDuring) return totalPublicMinted;
+    return 0;
+  }
+
+  function getTransactionCappedByMode() external view returns (uint256) {
     return
-      bytes(currentBaseURI).length > 0
-        ? string(
-          abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)
-        )
-        : "";
+      getState() == SaleState.PrivateSaleDuring
+        ? maxPrivateSalePerTx
+        : maxPublicSalePerTx;
   }
 
-  function mint(address to, uint256 amount) public {
-    require(totalSupply() + amount <= maxSupply, "Over max supply");
-    _safeMint(to, amount);
+  function availableForSale() external view returns (uint256) {
+    return maxSupply - totalSupply();
   }
 
-  function _beforeTokenTransfers(
-    address from,
-    address to,
-    uint256 startTokenId,
-    uint256 quantity
-  ) internal override {
-    super._beforeTokenTransfers(from, to, startTokenId, quantity);
+  function getPriceByMode() public view returns (uint256) {
+    if (getState() == SaleState.PrivateSaleDuring) return privateSalePrice;
+
+    return publicSalePrice;
   }
 
-  // The following functions are overrides required by Solidity.
   function supportsInterface(bytes4 interfaceId)
     public
     view
-    override(ERC721A)
+    virtual
+    override(ERC721, ERC721Enumerable)
     returns (bool)
   {
     return super.supportsInterface(interfaceId);
   }
 
-  function _startTokenId() internal view virtual override returns (uint256) {
-    return 1;
+  function startPublicSaleBlock() external view returns (uint256) {
+    return publicSale.beginBlock;
+  }
+
+  function endPublicSaleBlock() external view returns (uint256) {
+    return publicSale.endBlock;
+  }
+
+  function startPrivateSaleBlock() external view returns (uint256) {
+    return privateSale.beginBlock;
+  }
+
+  function endPrivateSaleBlock() external view returns (uint256) {
+    return privateSale.endBlock;
+  }
+
+  function withdraw() external onlyOwner {
+    uint256 balance = address(this).balance;
+    payable(msg.sender).transfer(balance);
+  }
+
+  function _mintToken(address addr, uint256 amount) internal returns (bool) {
+    for (uint256 i = 0; i < amount; i++) {
+      uint256 tokenIndex = totalSupply();
+      if (tokenIndex < maxSupply) _safeMint(addr, tokenIndex + 1);
+    }
+    return true;
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal virtual override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
   }
 }
---
+++ contracts/lib/BlockBasedSale.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract BlockBasedSale is Ownable {
+  using SafeMath for uint256;
+
+  enum OverrideSaleState {
+    None,
+    Pause,
+    Close
+  }
+
+  enum SalePhase {
+    None,
+    Private,
+    Public
+  }
+
+  OverrideSaleState public overridedSaleState = OverrideSaleState.None;
+  SalePhase public salePhase = SalePhase.None;
+
+  uint256 public maxPrivateSalePerTx = 10;
+  uint256 public maxPublicSalePerTx = 20;
+
+  uint256 public privateSaleCapped = 690;
+  uint256 public totalPrivateSaleMinted = 0;
+  uint256 public privateSalePrice;
+
+  uint256 public totalPublicMinted = 0;
+  uint256 public totalReserveMinted = 0;
+  uint256 public maxSupply = 6969;
+  uint256 public maxReserve = 169;
+
+  uint256 public publicSalePrice;
+
+  struct SaleConfig {
+    uint256 beginBlock;
+    uint256 endBlock;
+  }
+
+  SaleConfig public privateSale;
+  SaleConfig public publicSale;
+
+	constructor(SaleConfig memory _privateSale, SaleConfig memory _publicSale) {
+		privateSale = _privateSale;
+		publicSale = _publicSale;
+	}
+
+  function setTransactionLimit(
+    uint256 privateSaleLimit,
+    uint256 publicSaleLimit
+  ) external onlyOwner {
+    require(privateSaleLimit > 0);
+    require(publicSaleLimit > 0);
+    maxPrivateSalePerTx = privateSaleLimit;
+    maxPublicSalePerTx = publicSaleLimit;
+  }
+
+  function setPrivateSaleConfig(SaleConfig memory _privateSale)
+    external
+    onlyOwner
+  {
+    privateSale = _privateSale;
+  }
+
+  function setPublicSaleConfig(SaleConfig memory _publicSale)
+    external
+    onlyOwner
+  {
+    publicSale = _publicSale;
+  }
+
+  function setPublicSalePrice(uint256 _price) external onlyOwner {
+    publicSalePrice = _price;
+  }
+
+  function setPrivateSalePrice(uint256 _price) external onlyOwner {
+    privateSalePrice = _price;
+  }
+
+  function setCloseSale() external onlyOwner {
+    overridedSaleState = OverrideSaleState.Close;
+  }
+
+  function setPauseSale() external onlyOwner {
+    overridedSaleState = OverrideSaleState.Pause;
+  }
+
+  function resetOverridedSaleState() external onlyOwner {
+    overridedSaleState = OverrideSaleState.None;
+  }
+
+  function setReserve(uint256 reserve) external onlyOwner {
+    maxReserve = reserve;
+  }
+
+  function setPrivateSaleCap(uint256 cap) external onlyOwner {
+    privateSaleCapped = cap;
+  }
+
+  function isPrivateSaleSoldOut() external view returns (bool) {
+    return totalPrivateSaleMinted == privateSaleCapped;
+  }
+
+  function isPublicSaleSoldOut() external view returns (bool) {
+    uint256 supplyWithoutReserve = maxSupply - maxReserve;
+    uint256 mintedWithoutReserve = totalPublicMinted + totalPrivateSaleMinted;
+    return supplyWithoutReserve == mintedWithoutReserve;
+  }
+
+  function enablePublicSale() external onlyOwner {
+    salePhase = SalePhase.Public;
+  }
+
+  function enablePrivateSale() external onlyOwner {
+    salePhase = SalePhase.Private;
+  }
+}
